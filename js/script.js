e665eaccaa20616e18eb9e12f8045219b44c89c9
Laura Londo <lauralondo11@gmail.com>
2017-07-04 07:38:08 +0000

    added step backward, seek forward, previous episode, next episode, volume up, volume down, key press detection, progress bar, buffer tracking

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
+var feed = "http://adventurezone.libsyn.com/rss";
+var player = $('#player');
+var currentEpisode;
 
 
 $(document).ready( function() {
-    var feed = "http://adventurezone.libsyn.com/rss";
-    var player = $('#player');
-
 
     //get rss feed information for this podcast
     $.ajax(feed, {
@@ -33,60 +33,13 @@ $(document).ready( function() {
 
 	    //on click for list episode
 	    $(".list-episode-row").on('click', function() {
-		var url = $(this).attr("data-file-url"); 
 		console.log('clicked ' + $(this).find(".list-episode-title").html());
-		$('.list-episode-row').removeClass('playing'); //remove playing class from other episodes
-		$(this).addClass('playing'); //add playing class to this episode
-		player.attr('src', url); //set audio source url
-
-		player[0].play(); //play audio		
-		
+		playEpisode($(this));
 	    });
 	}//end success function
     });//end ajax call
 
 
-
-    //when player metadata loaded, display total time in player
-    player[0].onloadedmetadata = function() {
-	$("#time-total").html(secondsToTimeString(player[0].duration));
-    };
-    
-    
-
-    //on time update, display current time in player
-    player[0].ontimeupdate = function() {
-	var currentSeconds = player[0].currentTime;
-	
-	//display current time    
-	var stringTime = secondsToTimeString(currentSeconds);
-	$("#time-current").html(stringTime);
-	
-	//move progress bar
-	var percent = (currentSeconds / player[0].duration)*100;
-	$('#progress').width(percent + "%");
-    };
-
-    
-    // play or pause audio when play or pause button is clicked
-    $("#play-pause").on('click', function() {
-	if(player[0].paused) player[0].play(); //if paused, play
-	else player[0].pause(); //else, pause
-    });
-
-
-    //when player plays, change button to "pause"
-    player[0].onplay = function() {
-	$("#play-pause").removeClass("glyphicon-play");
-	$("#play-pause").addClass("glyphicon-pause");
-    }
-    
-    //when player pauses, change button to "play"
-    player[0].onpause = function() {
-	$("#play-pause").removeClass("glyphicon-pause");
-	$("#play-pause").addClass("glyphicon-play");
-    }
-    
 });//end document ready function
 
 
@@ -94,6 +47,196 @@ $(document).ready( function() {
 
 
 
+//Audio Player Button Click Events=========================================
+
+// play or pause audio when play or pause button is clicked
+$("#play-pause").on('click', function() {
+    togglePause();
+});
+$("#step-backward").on('click', function() {
+    stepBackward(10);
+});
+$("#step-forward").on('click', function() {
+    stepForward(20);
+});
+$("#previous").on('click', function() {
+    previous();
+});
+$("#next").on('click', function() {
+    next();
+});
+
+
+$(document).keydown(function(e) {
+    console.log(e.which);
+    if(e.which == 32) { //spacebar (pause)
+	e.preventDefault();
+	togglePause();
+    }
+    else if(e.which == 37) { //left arrow (step backward)
+	e.preventDefault();
+	stepBackward();
+    }
+    else if(e.which == 39) { //right arrow (step forward)
+	e.preventDefault();
+	stepForward();
+    }
+    else if(e.which == 38) { //up arrow (volume up)
+	e.preventDefault();
+	volumeUp();
+    }
+    else if(e.which == 40) { //down arrow (volume down)
+	e.preventDefault();
+	volumeDown();
+    }
+    
+
+});
+
+
+
+
+
+
+
+
+//player functions =================================================
+
+//play the given episode (given jquery episode object)
+function playEpisode(episode) {
+    currentEpisode = episode; //set current episode
+    var url = episode.attr("data-file-url"); //get audio source url
+    //change episode background color
+    $('.list-episode-row').removeClass('playing'); //remove playing class from other eps
+    episode.addClass('playing'); //add playing class to this episode
+    setSource(url); //set audio source
+    play(); //play audio  
+}
+
+//set audio source url
+function setSource(url) {
+    player.attr('src', url); //set audio source url
+    console.log("set source url");
+}
+//play audio
+function play() {
+    player[0].play();
+    console.log("play");
+}
+//play or pause audio depending on current state
+function togglePause() {
+    if(player[0].paused) { //if paused, play
+	player[0].play();
+	console.log("play")
+    }
+    else { //else, not paused, so pause
+	player[0].pause();
+	console.log("pause")
+    }
+}
+
+//step backwards by the given number of seconds
+function stepBackward(seconds=10) {
+    //if(seconds == null) seconds = 10;
+    player[0].currentTime -= seconds
+    console.log("step backward " + seconds + " seconds");
+}
+
+//step forward by the given number of seconds
+function stepForward(seconds=30) {
+    player[0].currentTime += seconds;
+    console.log("step forward " + seconds + " seconds");
+}
+
+//NOTE: how do we deal with prev & next if the dom list changes? episode no longer in dom...
+//play next episode if there is one
+function next() {
+    var next = currentEpisode.next();
+    if(next.length != 0) {
+	playEpisode(currentEpisode.next());
+	console.log("next episode");
+	return next;
+    }
+    console.log("no next episode");
+    return null;
+}
+
+//play previous episode if there is one
+function previous() {
+    var prev = currentEpisode.prev();
+    if(prev.length != 0) {
+	playEpisode(currentEpisode.prev());
+	console.log("previous episode");
+	return prev;
+    }
+    console.log("no previous episode");
+    return null;
+}
+
+function volumeUp() {
+    player[0].volume += 0.1;
+}
+function volumeDown() {
+    player[0].volume -= 0.1;
+}
+
+
+
+
+
+//player events =================================================
+
+//when player plays, change button to "pause"
+player[0].onplay = function() {
+    $("#play-pause").removeClass("glyphicon-play");
+    $("#play-pause").addClass("glyphicon-pause");
+};
+//when player pauses, change button to "play"
+player[0].onpause = function() {
+    $("#play-pause").removeClass("glyphicon-pause");
+    $("#play-pause").addClass("glyphicon-play");
+};
+
+//when player metadata loaded, display total time in player
+player[0].onloadedmetadata = function() {
+    $("#time-total").html(secondsToTimeString(player[0].duration));
+};
+//when player finishes the current media, play the next in the list
+player[0].onended = function() {
+    next();
+};
+
+//on time update, display current time in player
+player[0].ontimeupdate = function() {
+    var currentSeconds = player[0].currentTime;
+    //display current time    
+    var stringTime = secondsToTimeString(currentSeconds);
+    $("#time-current").html(stringTime);
+    //move progress bar
+    var percent = (currentSeconds / player[0].duration)*100;
+    $('#progress').width(percent + '%');
+};
+
+//as media is downloaded (buffered), update buffer bar
+player[0].onprogress = function() {
+    //var bufferEnd = player[0].buffered.end( player[0].buffered.length-1 );
+    var bufferEnd = player[0].buffered.end(player[0].buffered.length-1);
+    var percent = (bufferEnd / player[0].duration) * 100;
+    //move buffer bar
+    $('#buffer').width(percent + '%');
+};   
+
+//when volume is changed, update volume display
+player[0].onvolumechange = function() {
+    //TODO: move volube slider
+};
+
+
+
+
+
+
+//other functions================================================================
 
 //change time in seconds into string format (hh:)mm:ss (hours only if  hours > 0) 
 function secondsToTimeString(totalSeconds) {
@@ -113,5 +256,3 @@ function secondsToTimeString(totalSeconds) {
     
     return timeString;
 }
-
-
